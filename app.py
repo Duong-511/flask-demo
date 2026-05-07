869920f9027aa4561775cf4247e1f4658c8c047d
Duong <nbduong511#gmail.com>
2026-05-07 22:19:37 +0700

    Cập nhật code

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask, request
 import sqlite3
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(32)
-csrf = CSRFProtect(app)
-
 password = "123456"
 
+@app.route('/')
+def index():
+    
+    db = sqlite3.connect('database.db')
+    return "Hệ thống đang hoạt động!"
+
+if __name__ == '__main__':
+    
+    app.run(host='0.0.0.0', port=5000)
