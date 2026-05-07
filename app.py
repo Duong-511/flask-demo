018dfc5d590eb03f42b96567d4c0ad95fb4fc75e
Duong <nbduong511#gmail.com>
2026-05-07 22:15:39 +0700

    Cập nhật code

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request
-from flask_wtf.csrf import CSRFProtect
 import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(32)
 csrf = CSRFProtect(app)
 
-password = os.getenv("password") # Compliant
+password = "123456"
 
