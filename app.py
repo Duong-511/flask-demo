e4a4219a4627132c6478a7f07e226ff8ab09391f
Duong <nbduong511#gmail.com>
2026-05-07 22:13:48 +0700

    Cập nhật code

--- app.py
+++ app.py
@@ -8,18 +8,3 @@ csrf = CSRFProtect(app)
 
 password = os.getenv("password") # Compliant
 
-@app.route('/user')
-def get_user():
-
-    user_id = request.args.get('id')
-
-    conn = sqlite3.connect('test.db')
-    cursor = conn.cursor()
-
-    query = f"SELECT * FROM users WHERE id = '{user_id}'"
-
-    cursor.execute(query)
-
-    return "done"
-
-app.run(host='0.0.0.0', port=5000)
