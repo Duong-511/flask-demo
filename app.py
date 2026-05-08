62ac27c4fe17f5238ee7a8fe57d020facf5c134e
Duong <nbduong511#gmail.com>
2026-05-08 10:51:29 +0700

    sql injection

--- app.py
+++ app.py
@@ -2,18 +2,27 @@ from flask import Flask, request
 import sqlite3
 
 app = Flask(__name__)
-csrf = CSRFProtect()
-csrf.init_app(app)
 
-password = os.getenv("password")
+password = "123456"
 
 @app.route('/')
 def index():
-    
     db = sqlite3.connect('database.db')
-    db.close()
+    
     return "Hệ thống đang hoạt động!"
 
+@app.route('/user')
+def get_user():
+    user_id = request.args.get('id')
+
+    conn = sqlite3.connect('test.db')
+
+    query = "SELECT * FROM users WHERE id = " + user_id
+
+    result = conn.execute(query)
+
+    return str(result.fetchall())
+
 if __name__ == '__main__':
-    
+
     app.run(host='0.0.0.0', port=5000)
